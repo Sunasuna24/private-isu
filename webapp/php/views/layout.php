4331f9f3bdf7a2e5af2cc3c9ab2999ef210862b6
Sunasuna24 <sunasunayaka1218@gmail.com>
2023-11-18 16:57:11 +0900

    Webアプリのタイトルを編集した

--- webapp/php/views/layout.php
+++ webapp/php/views/layout.php
@@ -2,7 +2,8 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Iscogram</title>
+    <!-- <title>Iscogram</title> -->
+    <title>Iscogram2</title>
     <link href="/css/style.css" media="screen" rel="stylesheet" type="text/css">
   </head>
   <body>

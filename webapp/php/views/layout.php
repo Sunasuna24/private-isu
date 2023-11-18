c4252b5341be2e673fdeec961ac53f82ce416e36
Sunasuna24 <sunasunayaka1218@gmail.com>
2023-11-18 17:06:05 +0900

    titleを元に戻した

--- webapp/php/views/layout.php
+++ webapp/php/views/layout.php
@@ -2,8 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <!-- <title>Iscogram</title> -->
-    <title>Iscogram2</title>
+    <title>Iscogram</title>
     <link href="/css/style.css" media="screen" rel="stylesheet" type="text/css">
   </head>
   <body>
